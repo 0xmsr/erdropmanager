afa50701f59122987119baad62cf9fe44e838789
0xmsr <148552427+0xmsr@users.noreply.github.com>
2026-01-31 18:08:45 +0700

    Add Waitlist and NotFound routes to App component

--- v2/src/App.tsx
+++ v2/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Finance } from './pages/Finance';
 import { Faucet } from './pages/Faucet';
+import { Waitlist } from './pages/Waitlist';
+import { NotFound } from './pages/NotFound';
 import './App.css';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/finance" element={<Finance />} />
         <Route path="/faucet" element={<Faucet />} />
+        <Route path="/waitlist" element={<Waitlist />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
